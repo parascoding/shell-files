d3b53f2fe06c97bc353125a7c2e5f60ebf74337c
parascoding <parascoding@gmail.com>
2022-08-07 19:19:35 +0530

    Final

--- shell.sh
+++ shell.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # Creating a direcotry using "mkdir"
 mkdir dir
-echo "Directory named dir has been created"
+echo "Message: ----------> Directory named dir has been created"
 
 # Moving to the newly created directory using the cd command
 cd dir
-echo "Move to dir"
+echo "Message: ----------> Move to dir"
 
 # Creating some files using the touch command and loop
 i=0
@@ -15,20 +15,44 @@ do
     file="$file$i.txt"
     touch $file
     i=`expr $i + 1`
-    echo "$file has been created in dir"
+    echo "Message: ----------> $file has been created in dir"
 done
 
 # Listing all the direcotries created using the ls command
-echo "File in $pwd are"
+echo "Message: ----------> Files are"
 ls -al
 
 
+# Creating another directory named child
+mkdir child
+echo "Message: ----------> Child directory created"
 
+# Moving some files from dir to child
+mv file1.txt file2.txt child
+echo "file1 and file2 have been moved to child"
 
+# Moving to child
+cd child
+echo "Message: ----------> Moved to child"
 
+# Printing files in child
+echo "Files in child are"
+ls -al
 
+# Moving back to dir
+cd ../
+echo "Message: ----------> Moved back to dir"
 
+# Compressing child
+tar -czf child.tar.gz child
+echo "Message: ----------> child have been compressed"
 
+# Removing directory child
+rm -r child
+
+# Extracting child.gz
+tar -xf child.tar.gz
+echo "Message: ----------> child have been extracted"
 
 # Moving to the base directory and deleting all the changes made
 cd ../
