4d6e35934e1260e5b23120c242b5daafa326ee59
Paras Yadav <62833829+parascoding@users.noreply.github.com>
2022-10-19 16:20:18 +0530

    Add files via upload

---
+++ Shell.sh
@@ -0,0 +1,16 @@
+apt update
+apt install fish -y
+apt install gcc -y
+apt install g++ -y
+apt install snapd-xdg-open
+snap install code --classic
+apt install default-jdk -y
+apt install gnome-tweaks -y
+apt install git -y
+apt install gnome-shell-extensions -y
+apt install chrome-gnome-shell -y
+apt install curl -y
+apt install texlive-latex-recommended -y
+fish
+curl -L https://get.oh-my.fish | fish
+omf install toaster
